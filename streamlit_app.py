c9fdbac79278baea24be306a49ed2a5d1a9e7118
praveen22laxman <90193659+praveen22laxman@users.noreply.github.com>
2024-08-08 18:27:20 +0530

    Update streamlit_app.py
    changes needed bc of move from sis to snis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-session = get_active_session()
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -14,6 +11,8 @@ Name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", Name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
  #st.dataframe(data=my_dataframe, use_container_width=True)
+cnx=st.connection("snowflake")
+session = cnx.session()
 ingredients_list=st.multiselect('Choose upto 5 ingredients:',my_dataframe,max_selections=5)
 
 if ingredients_list:
